6a0d7fb2e87c47197f2ba28b8fb59ef12d5413b1
Jeroen Offerijns <jeroen@offerijns.com>
2020-03-04 14:31:03 +0100

    feat: add char level lstm

--- model/lstm_char.py
+++ model/lstm_char.py
@@ -5,81 +5,67 @@ import torch.optim as optim
 import torch.autograd as autograd
 
 class LSTMCharTagger(nn.Module):
-    def __init__(self, embedding_dim_words, embedding_dim_chars, hidden_dim_words, hidden_dim_chars, vocab_size, tagset_size, charset_size):
-        """LSTM Part-of-Speech Tagger Augmented with Character level features
-        
-        Atttributes:
-            embedding_dim_words: Embedding dimension of word features to input to LSTM word level
-            embedding_dim_chars: Embedding dimension of word features to input to character level
-            hidden_dim_words: Output size of the LSTM word level
-            hidden_dim_chars: Output size of the LSTM character level
-            vocab_size: Size of the vocabulary of characters
-            tagset_size: Size of the set of labels
-            charset_size: Size of the vocabulary of characters
-        """
+    """LSTM part-os-speech (PoS) tagger."""
+
+    def __init__(self, word_embedding_dim, char_embedding_dim, char_repr_dim, hidden_dim, vocab_size, chars_size, tagset_size, word_to_ix, char_to_ix, make_ixs):
         super(LSTMCharTagger, self).__init__()
-        self.hidden_dim_words = hidden_dim_words
-        self.hidden_dim_chars = hidden_dim_chars
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim_words)
-        self.char_embeddings = nn.Embedding(charset_size, embedding_dim_chars)
-        self.lstm_char = nn.LSTM(embedding_dim_chars, hidden_dim_chars)
-        self.lstm_words = nn.LSTM(embedding_dim_words + hidden_dim_chars, hidden_dim_words)
-        self.hidden2tag = nn.Linear(hidden_dim_words, tagset_size)
-        self.hidden_char = self.init_hidden(c=False)
-        self.hidden_words = self.init_hidden(c=True)
-    
-    def init_hidden(self, c=True):
-        """Initialize hidden state of LSTMs
+        self.char_repr_dim = char_repr_dim
+        self.hidden_dim = hidden_dim
         
-        Args:
-            c(boolean): return initialized hidden state for LSTM word level if true
-        
-        """
-        if c:
-            return (autograd.Variable(torch.zeros(1, 1, self.hidden_dim_words)),
-                    autograd.Variable(torch.zeros(1, 1, self.hidden_dim_words)))
-        else:
-            return (autograd.Variable(torch.zeros(1, 1, self.hidden_dim_chars)),
-                    autograd.Variable(torch.zeros(1, 1, self.hidden_dim_chars)))
-    
-    
-    def forward(self, sentence_seq, words_tensor_dict):
-        """Forward propagation
-        
-        Args:
-            sentence_seq(list): Sequence of indexis related to the corresponding sentence words
-            words_tensor_dict(dict): Dictionary of tensors of words at the character level
-        
-        Returns:
-            tensor: Labels predicted (POS) for the sequence
-        """
-        # embeds = self.word_embeddings(sentence)
-        for ix, word_idx in enumerate(sentence_seq):
-            
-            # Char level
-            word_chars_tensors = words_tensor_dict[int(word_idx)]
-            char_embeds = self.char_embeddings(word_chars_tensors)
-            
-            # Remember that the input of LSTM is a 3D Tensor:
-            # The first axis is the sequence itself, 
-            # the second indexes instances in the mini-batch, and 
-            # the third indexes elements of the input.
-            lstm_char_out, self.hidden_char = self.lstm_char(
-                char_embeds.view(len(char_embeds), 1, -1), self.hidden_char)
-            
-            # Word level
-            embeds = self.word_embeddings(word_idx)
-            # Now here we will only keep the final hidden state of the character level LSTM
-            # i.e lstm_char_out[-1]
-            embeds_cat = torch.cat((embeds, lstm_char_out[-1]), dim=1)
-            
-            lstm_out, self.hidden_words = self.lstm_words(embeds_cat, self.hidden_words)
-            tag_space = self.hidden2tag(lstm_out.view(1, -1))
-            
-            tag_score = F.log_softmax(tag_space, dim=1)
-            if ix==0:
-                tag_scores = tag_score
-            else:
-                tag_scores = torch.cat((tag_scores, tag_score), 0)
-        
+        self.word_to_ix = word_to_ix
+        self.char_to_ix = char_to_ix
+        self.make_ixs = make_ixs
+
+        self.word_embeddings = nn.Embedding(vocab_size, word_embedding_dim)
+        self.char_embeddings = nn.Embedding(chars_size, char_embedding_dim)
+
+        self.char_lstm = nn.LSTM(char_embedding_dim, char_repr_dim)
+        self.word_lstm = nn.LSTM(word_embedding_dim + char_repr_dim, hidden_dim)
+
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+
+        self.char_lstm_hidden = (torch.zeros(1, 1, self.char_repr_dim),
+                                 torch.zeros(1, 1, self.char_repr_dim))
+        self.word_lstm_hidden = (torch.zeros(1, 1, self.hidden_dim),
+                                 torch.zeros(1, 1, self.hidden_dim))
+
+    def init_word_hidden(self):
+        """Initialise word LSTM hidden state."""
+        self.word_lstm_hidden = (torch.zeros(1, 1, self.hidden_dim),
+                                 torch.zeros(1, 1, self.hidden_dim))
+
+    def init_char_hidden(self):
+        """Initialise character LSTM hidden state."""
+        self.char_lstm_hidden = (torch.zeros(1, 1, self.char_repr_dim),
+                                 torch.zeros(1, 1, self.char_repr_dim))
+
+    def forward(self, sentence):
+        sentence_length = len(sentence)
+        word_characters_ixs = {}
+        for word in sentence:
+            word_ix = torch.tensor([self.word_to_ix[word]])
+            char_ixs = self.make_ixs(word, self.char_to_ix)
+            word_characters_ixs[word_ix] = char_ixs
+
+        word_embeds = []
+        for word_ix, char_ixs in word_characters_ixs.items():
+            word_embed = self.word_embeddings(word_ix)
+
+            self.init_char_hidden()
+            c = None  # Character-level representation.
+            # Character-level representation is the LSTM output of the last character.
+            for char_ix in char_ixs:
+                char_embed = self.char_embeddings(char_ix)
+                c, self.char_lstm_hidden = self.char_lstm(
+                    char_embed.view(1, 1, -1), self.char_lstm_hidden)
+            word_embeds.append(word_embed)
+            word_embeds.append(c.view(1, -1))
+        word_embeds = torch.cat(word_embeds, 1)
+
+        lstm_out, self.word_lstm_hidden = self.word_lstm(
+            # Each sentence embedding dimensions are word embedding dimensions + character representation dimensions
+            word_embeds.view(sentence_length, 1, -1),
+            self.word_lstm_hidden)
+        tag_space = self.hidden2tag(lstm_out.view(sentence_length, -1))
+        tag_scores = F.log_softmax(tag_space, dim=1)
         return tag_scores
