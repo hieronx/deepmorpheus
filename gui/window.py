0ea90ee341bae0c24d30acb22bb18533c7319a36
Mees Gelein <meesgelein@gmail.com>
2020-07-01 12:11:41 +0200

    Working on layout

--- gui/window.py
+++ gui/window.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 
 class Window(Frame):
     """The window class that subclasses the TKinter Frame, this holds the entire program"""
@@ -7,8 +8,12 @@ class Window(Frame):
         """Creates the new window with a reference to its master"""
         Frame.__init__(self, master)
         self.master = master
-        
+        self.pack(fill=BOTH, expand=True)
         self.create_menu()
+
+        # Example on how to place buttons in relative positions
+        # button = Button(text='Click to Open File', command=self.open_file)
+        # button.pack(side=RIGHT, padx=5, pady=5)
 
     def create_menu(self):
         """Creates the menu on the top border of the window"""
@@ -22,6 +27,7 @@ class Window(Frame):
 
     def open_file(self):
         """Opens the file dialog window letting us point to a file to be opened as input"""
+        print(filedialog.askopenfilename())
 
     def exit_program(self):
         """Attempts a graceful shutdown of the Tkinter mainloop"""
