8f50934e409e0580c67aa3ee7849a06a93336367
Mees Gelein <meesgelein@gmail.com>
2020-07-02 00:33:50 +0200

    We're starting to get somewhere

--- gui/application.py
+++ gui/application.py
@@ -10,8 +10,9 @@ class Application(Frame):
         Frame.__init__(self, master)
         self.master = master
         self.mode = INPUT
-        self.input_file = StringVar(self.master)
-        self.input_file.set("No file loaded...")
+        self.filename = StringVar(self.master)
+        self.input_file = "No file loaded..."
+        self.filename.set(self.input_file)
         self.configure_grid()
         self.menu = self.create_menu()
         self.settings_frame, self.contents_frame, self.buttons_frame = self.create_frames()
@@ -24,39 +25,49 @@ class Application(Frame):
         """Populates the middle frame"""
         self.contents_frame.columnconfigure(0, weight=1)
         self.contents_frame.columnconfigure(1, weight=0)
-        self.contents_frame.rowconfigure(0, weight=1)
-        self.contents_frame.rowconfigure(1, weight=0)
+        self.contents_frame.rowconfigure(0, weight=0)
+        self.contents_frame.rowconfigure(1, weight=1)
         self.contents_frame.rowconfigure(2, weight=0)
 
-        file_label = Label(self.master, textvariable = self.input_file)
-        file_label.grid(row=2, column=0, columnspan=2, sticky=NESW)
+        self.file_label = Label(self.contents_frame, textvariable = self.filename)
+        self.file_label.grid(row=0, column=0, columnspan=2, sticky=NESW)
 
         self.input_text = Text(self.contents_frame, wrap=NONE)
-        self.output_text = Text(self.contents_frame, wrap=NONE)
-
+        self.input_text.bind_all('<Key>', self.check_input)
+        self.output_text = Text(self.contents_frame, wrap=NONE, bg='black', fg='white')
+        
         self.yscroll = Scrollbar(self.contents_frame)
-        self.yscroll.grid(row=0, column=1, sticky=NESW)
+        self.yscroll.grid(row=1, column=1, sticky=NESW)
 
         self.xscroll = Scrollbar(self.contents_frame, orient=HORIZONTAL)
-        self.xscroll.grid(row=1, column=0, sticky=NESW)
+        self.xscroll.grid(row=2, column=0, sticky=NESW)
         self.register_text_field(self.input_text)
+
+    def check_input(self, *args):
+        """Checks if we are ready to enable/disable the start button"""
+        input_text = self.input_text.get('1.0').strip()
+        new_state =  DISABLED if len(input_text) == 0 else NORMAL
+        self.start_button['state'] = new_state
 
     def register_text_field(self, textfield):
         """Registers the textfield to the scrollbars"""
         self.yscroll.config(command=textfield.yview)
         self.xscroll.config(command=textfield.xview)
         textfield.config(yscrollcommand=self.yscroll.set, xscrollcommand=self.xscroll.set)
-        textfield.grid(row = 0, column = 0, sticky=NESW)
+        textfield.grid(row = 1, column = 0, sticky=NESW)
 
     def populate_buttons(self):
         """Populates the bottom frame"""
         self.buttons_frame.columnconfigure(0, weight = 1)
         self.buttons_frame.columnconfigure(1, weight = 1)
         self.buttons_frame.columnconfigure(2, weight = 1)
+        self.buttons_frame.rowconfigure(0, weight = 1)
+        self.buttons_frame.rowconfigure(1, weight = 0)
 
         # Create the start conversion button
-        start_button = Button(self.buttons_frame, text='Start Analysis')
-        start_button.grid(row=0, column=2, sticky=NESW, padx=4, pady=4)
+        self.start_button = Button(self.buttons_frame, text='Start Analysis')
+        self.start_button.grid(row=0, column=2, sticky=NESW, padx=4, pady=4)
+        self.start_button['state'] = DISABLED
 
         # Create the toggle output buttons
         self.toggle_button = Button(self.buttons_frame, text=self.get_toggle_text(), command=self.toggle_text_mode)
@@ -70,9 +81,11 @@ class Application(Frame):
         if self.mode == INPUT:
             self.register_text_field(self.input_text)
             self.output_text.grid_forget()
+            self.filename.set(self.input_file)
         else:
             self.register_text_field(self.output_text)
             self.input_text.grid_forget()
+            self.filename.set('-- CONSOLE OUTPUT --')
 
     def get_toggle_text(self):
         """Returns the correct output for the toggle button"""
@@ -145,9 +158,11 @@ class Application(Frame):
         try:
             with open(filename, 'r', encoding='utf8') as f:
                 contents = "".join(f.readlines())
-            self.input_file.set(filename)
+            self.input_file = filename
+            self.filename.set(self.input_file)
+            self.start_button['state'] = NORMAL
         except:
-            self.input_file.set("Could not open file: %s" % filename)
+            self.input_file = "Could not open file: %s" % filename
             
         self.input_text.delete('1.0', END)
         self.input_text.insert('1.0', contents)
